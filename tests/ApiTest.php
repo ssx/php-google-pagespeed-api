e54d3e8d69198930d4fb801c904481789b8ff0c8
Scott Wilcox <scott@dor.ky>
2017-06-13 19:59:35 +0000

    Apply fixes from StyleCI

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of ssx/php-google-pagespeed-api
+ *
+ *  (c) Scott Wilcox <scott@dor.ky>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ */
+
 use PHPUnit\Framework\TestCase;
 use SSX\API\Google\PageSpeed\ApiClient;
 
